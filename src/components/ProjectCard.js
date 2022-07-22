3ba470d9cde263ea9a28619d1b949ef3e345a68f
oluroyleseyler <akkasumeyye@hotmail.com>
2022-07-22 16:15:00 +0300

    logolar eklendi

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import {AiFillGithub , AiOutlineLink} from "react-icons/ai"
+
 
 const SingleCard = styled.div`
   width: 20vw;
@@ -10,32 +12,37 @@ const SingleCard = styled.div`
   flex-direction: column;
   background-color: #156d39;
   border-radius: 1rem;
+  padding:3rem;
+
   &:hover {
     transform: translateY(-5%);
     box-shadow: 11px 11px 30px 3px rgba(0,0,0,0.36);
   }
 
-  h3{
-    margin-top:2rem;
-    margin-left:5vw;
-  }
+h4{
+  color: #b2ffff;
+}
 
   p{
     font-weight: 200;
+  }
+
+  svg {
+    color : #fff;
   }
 `;
 
 const ProjectCard = ({name, desc , tech, github ,page}) => {
   return (
     <SingleCard>
-    <h3>{name}</h3>
+    <h4>{name}</h4>
     <p>{desc}</p>
     <p>{tech.join(", ")}</p>
      <a href={github} rel="noreferrer" target="_blank">
-     <i className="fab fa-github icon fa-2x"></i>
+     <AiFillGithub/>
      </a>
      <a href={page} rel="noreferrer" target="_blank">
-     <i className="fas fa-link icon fa-2x"></i>
+     <AiOutlineLink/>
      </a>
     </SingleCard>
   )  
